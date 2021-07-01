5dd98ff40d2fae4a43056c86a388f99486faa437
Steffen Kasten <steffen.kasten@uni-rostock.de>
2021-07-01 12:53:13 +0200

    add messages

--- src/main/java/de/uni/rostock/ub/purl_server/validate/UserValidateService.java
+++ src/main/java/de/uni/rostock/ub/purl_server/validate/UserValidateService.java
@@ -2,7 +2,10 @@ package de.uni.rostock.ub.purl_server.validate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -13,6 +16,9 @@ public class UserValidateService {
 
     private static String SHA_EMPTY_STRING = "da39a3ee5e6b4b0d3255bfef95601890afd80709";
     
+    @Autowired
+    MessageSource messages;
+    
     /**
      * Validate the user
      * 
@@ -22,10 +28,10 @@ public class UserValidateService {
     public List<String> validateUser(User user) {
         List<String> errorList = new ArrayList<>();
         if (SHA_EMPTY_STRING.equals(user.getPasswordSHA())) {
-            errorList.add("Password can not be empty!");
+            errorList.add(messages.getMessage("purl_server.error.validate.user.password.empty", null, Locale.getDefault()));
         }
         if (StringUtils.isEmpty(user.getLogin())) {
-            errorList.add("Username can not be empty!");
+            errorList.add(messages.getMessage("purl_server.error.validate.user.username.empty", null, Locale.getDefault()));
         }
         return errorList;
     }

8401c081aff98feadcedf4801b4c7b75f172850c
mcradmin <mcradmin@UB1MM030.ub.uni-rostock.de>
2021-09-26 13:02:37 +0200

    update sql (create table statements)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO `user`
-  (`id`, `admin`, `fullname`, `affiliation`, `email`, `login`, `password_sha`, `password_reset_token`, `comment`, `created`, `lastmodified`, `status`) 
+  (`id`, `login`, `admin`, `fullname`, `affiliation`, `email`, `password_sha`, `password_reset_token`, `comment`, `created`, `lastmodified`, `status`) 
 VALUES
-  (null,  true, 'PURL Administrator', 'My Institution', 'admin@my-institution.org', 'admin', SHA1('admin'), null, 'main user for PURL server', NOW(), NOW(), 'CREATED')
+  (null,  'admin', true, 'PURL Administrator', 'My Institution', 'admin@my-institution.org', SHA1('admin'), null, 'main user for PURL server', NOW(), NOW(), 'CREATED')
 ON DUPLICATE KEY UPDATE lastmodified = NOW(), status = 'MODIFIED';

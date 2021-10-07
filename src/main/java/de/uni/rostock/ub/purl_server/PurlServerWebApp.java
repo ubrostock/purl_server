b9f085ca90f7a15bab6f74cddc3977aa7397af7b
Steffen Kasten <steffen.kasten@uni-rostock.de>
2021-10-07 16:37:09 +0200

    add language switcher

--- src/main/java/de/uni/rostock/ub/purl_server/PurlServerWebApp.java
+++ src/main/java/de/uni/rostock/ub/purl_server/PurlServerWebApp.java
@@ -18,13 +18,40 @@
  */
 package de.uni.rostock.ub.purl_server;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
-public class PurlServerWebApp {
+public class PurlServerWebApp implements WebMvcConfigurer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PurlServerWebApp.class, args);
-    }
+	@Bean(name = "localeResolver")
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.GERMAN);
+		return slr;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		return localeChangeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurlServerWebApp.class, args);
+	}
 }

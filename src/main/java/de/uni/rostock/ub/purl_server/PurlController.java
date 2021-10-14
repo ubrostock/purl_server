ef00f50f0aa799efc435c472e24258def0f05280
Steffen Kasten <steffen.kasten@uni-rostock.de>
2021-10-14 13:12:45 +0200

    #17 forward URL query parameters / error page for 410

--- src/main/java/de/uni/rostock/ub/purl_server/PurlController.java
+++ src/main/java/de/uni/rostock/ub/purl_server/PurlController.java
@@ -28,52 +28,81 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import de.uni.rostock.ub.purl_server.dao.PurlDAO;
 import de.uni.rostock.ub.purl_server.model.Purl;
-import de.uni.rostock.ub.purl_server.model.Type;
 
 @Controller
 public class PurlController {
 
-    @Autowired
-    PurlDAO purlDAO;
+	@Autowired
+	PurlDAO purlDAO;
 
-    @Autowired
-    private MessageSource messages;
-    
-    private static Logger LOGGER = LoggerFactory.getLogger(PurlController.class);
-    
-    /**
-     * Resolve the Purl
-     * 
-     * @param request the HttpServletRequest
-     * @param resp    the HttpServletResponse
-     * @param domain  the PathVariable
-     * @return redirect to the target URL
-     */
-    @RequestMapping(path = "/{domain:(?!admin)(?!api)(?!info)(?!webjars).*}/**", method = RequestMethod.GET)
-    public String resolvePurl(HttpServletRequest request, HttpServletResponse resp, @PathVariable String domain) {
-        String path = request.getServletPath();
-        Optional<Purl> op = purlDAO.retrievePurl(path);
-        if (op.isPresent()) {
-            Purl p = op.get();
-            if (p.getType() == Type.PARTIAL_302) {
-                String restPath = path.substring(p.getPath().length());
-                return "redirect:" + p.getTarget() + restPath;
-            }
-            return "redirect:" + p.getTarget();
-        } else {
-            try {
-                resp.sendError(HttpServletResponse.SC_NOT_FOUND, messages.getMessage("purl_server.error.purl.found", null, Locale.getDefault()));
-            } catch (IOException e) {
-                LOGGER.error(messages.getMessage("purl_server.error.sending.error", null, Locale.getDefault()), e);
-            }
-            return null;
-        }
-    }
+	@Autowired
+	private MessageSource messages;
+
+	private static Logger LOGGER = LoggerFactory.getLogger(PurlController.class);
+
+	/**
+	 * Resolve the Purl
+	 * 
+	 * @param request the HttpServletRequest
+	 * @param resp    the HttpServletResponse
+	 * @param domain  the PathVariable
+	 * @return redirect to the target URL
+	 */
+	@RequestMapping(path = "/{domain:(?!admin)(?!api)(?!info)(?!webjars).*}/**", method = RequestMethod.GET)
+	public String resolvePurl(HttpServletRequest request, HttpServletResponse resp, @PathVariable String domain) {
+		String path = request.getServletPath();
+		Optional<Purl> op = purlDAO.retrievePurl(path);
+		if (op.isPresent()) {
+			Purl p = op.get();
+			switch (p.getType()) {
+			case PARTIAL_302:
+				String restPath = path.substring(p.getPath().length());
+				return "redirect:" + calcRedirectWithParams(request, p.getTarget(), restPath);
+			case FOUND_302:
+				return "redirect:" + calcRedirectWithParams(request, p.getTarget(), "");
+			case GONE_410:
+				try {
+					resp.sendError(HttpServletResponse.SC_GONE,
+							messages.getMessage(
+									"purl_server.error.purl_deleted", new Object[] { ServletUriComponentsBuilder
+											.fromCurrentContextPath().path("/info/purl" + path).build().toString() },
+									Locale.getDefault()));
+				} catch (NoSuchMessageException | IOException e) {
+					LOGGER.error(messages.getMessage("purl_server.error.sending.error", null, Locale.getDefault()), e);
+				}
+				break;
+			}
+		} else {
+			try {
+				resp.sendError(HttpServletResponse.SC_NOT_FOUND,
+						messages.getMessage("purl_server.error.purl.found", null, Locale.getDefault()));
+			} catch (IOException e) {
+				LOGGER.error(messages.getMessage("purl_server.error.sending.error", null, Locale.getDefault()), e);
+			}
+		}
+		return null;
+	}
+
+	private String calcRedirectWithParams(HttpServletRequest request, String path, String restPath) {
+		String redirect = path + restPath;
+		String queryString = request.getQueryString();
+		if (StringUtils.hasText(queryString)) {
+			if (redirect.contains("?")) {
+				redirect = redirect + "&" + queryString;
+			} else {
+				redirect = redirect + "?" + queryString;
+			}
+		}
+		return redirect;
+	}
 }

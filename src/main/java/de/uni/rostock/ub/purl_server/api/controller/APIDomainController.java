bfb567f18e3065152f2597a47a7399e0a24dacef
Robert Stephan <robert.stephan@uni-rostock.de>
2022-08-14 15:50:41 +0200

    annotate domain controller #35

--- src/main/java/de/uni/rostock/ub/purl_server/api/controller/APIDomainController.java
+++ src/main/java/de/uni/rostock/ub/purl_server/api/controller/APIDomainController.java
@@ -22,19 +22,27 @@ import java.util.concurrent.atomic.AtomicReference;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.util.AntPathMatcher;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import de.uni.rostock.ub.purl_server.dao.DomainDAO;
 import de.uni.rostock.ub.purl_server.dao.UserDAO;
 import de.uni.rostock.ub.purl_server.model.Domain;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
-@Controller
+@RestController
 public class APIDomainController {
     @Autowired
     UserDAO userDAO;;
@@ -44,19 +52,23 @@ public class APIDomainController {
 
     /**
      * Retrieve the domain
-     * 
-     * @statuscode 200 if the domain was found
-     * @statuscode 404 if the domain does not exist return the ResponseEntity object
-     *             with the retrieved domain
      */
-    @RequestMapping("/api/domain/**")
-    public ResponseEntity<Domain> retrieveDomain(HttpServletRequest request) {
-        String domainName = "/"
-            + new AntPathMatcher().extractPathWithinPattern("/api/purl/**", request.getRequestURI());
+    @RequestMapping(path = "/api/domain/{path}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "OK", 
+            headers = @Header(name="Content-Location", 
+                              description = "the URL of the resource.", 
+                              schema = @Schema(type="string", format="url")),
+            content = @Content(schema = @Schema(implementation = Domain.class))),
+        @ApiResponse(responseCode = "404", description = "Not Found! A domain with the given path does not exist.", 
+                     content = @Content(schema = @Schema(hidden = true)))
+    })
+    public ResponseEntity<Domain> retrieveDomain(HttpServletRequest request,
+        @PathVariable("path") String path) {
         AtomicReference<ResponseEntity<Domain>> r = new AtomicReference<>();
-        domainDAO.retrieveDomain(domainName).ifPresentOrElse(d -> {
+        domainDAO.retrieveDomain(StringUtils.prependIfMissing(path,  "/")).ifPresentOrElse(d -> {
             HttpHeaders headers = new HttpHeaders();
-            headers.add("Location", request.getRequestURL().toString());
+            headers.add("Content-Location", request.getRequestURL().toString());
             r.set(new ResponseEntity<Domain>(d, headers, HttpStatus.OK));
         }, () -> {
             r.set(new ResponseEntity<Domain>(HttpStatus.NOT_FOUND));

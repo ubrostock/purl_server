3ca94dcff170b450f70c07615bfcc9b5a0acaf34
skasten-de <47594468+skasten-de@users.noreply.github.com>
2022-10-07 12:06:23 +0200

    add package for all controller #56 (#57)

--- src/main/java/de/uni/rostock/ub/purl_server/controller/admin/AdminIndexController.java
+++ src/main/java/de/uni/rostock/ub/purl_server/controller/admin/AdminIndexController.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.uni.rostock.ub.purl_server.admin.controller;
+package de.uni.rostock.ub.purl_server.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/de/uni/rostock/ub/purl_server/controller/api/APIUserController.java
+++ src/main/java/de/uni/rostock/ub/purl_server/controller/api/APIUserController.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.uni.rostock.ub.purl_server.api.controller;
+package de.uni.rostock.ub.purl_server.controller.api;
 
 import java.util.Optional;
 

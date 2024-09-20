0498c7dcb71e5ec9958faacbc100fcbf65558b19
Robert Stephan <robert.stephan@uni-rostock.de>
2024-09-20 15:46:34 +0200

    first test for domains

--- src/test/java/de/uni/rostock/ub/purl_server/test/DomainUnitTests.java
+++ src/test/java/de/uni/rostock/ub/purl_server/test/DomainUnitTests.java
@@ -1,6 +1,7 @@
 package de.uni.rostock.ub.purl_server.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
@@ -16,27 +17,40 @@ import de.uni.rostock.ub.purl_server.model.Domain;
 import de.uni.rostock.ub.purl_server.validate.DomainValidateService;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-public class DomainUnitTests {
+public class DomainUnitTests extends PURLServerBaseTest {
     @Autowired
     DomainValidateService domainValidateService;
 
     @Autowired
     DomainDAO domainDAO;
-
+    
     @Test
-    public void testDomainCreate() throws Exception {
+    public void testDomainValidate() throws Exception {
         Domain domain = new Domain();
         domain.setName("Test Domain 1");
         domain.setPath("/test1");
-        List<String> errorList = domainValidateService.validateCreateDomain(domain, Locale.GERMAN);
+        List<String> errorList = null;
+        errorList = domainValidateService.validateCreateDomain(domain, Locale.GERMAN);
         assertTrue(errorList.isEmpty(), "Domain invalid: " + String.join(", ", errorList));
+        
+        domain.setPath("/test 1");
+        errorList = domainValidateService.validateCreateDomain(domain, Locale.GERMAN);
+        assertFalse(errorList.isEmpty(), "Domain error not detected - given path is invalid");
+        assertTrue(errorList.get(0).startsWith("Pfad darf nur"), "Domain error message for invalid path not created");
+
+        // Hinweis/Beispiel: Test auf Liste mit Inhalt in beliebiger Reihenfolge
+        // Assertions.assertThat(expectedUserList).containsExactlyInAnyOrderElementsOf(actualUserList);
+    }
+
+    @Test
+    public void testDomainDAOCreate() throws Exception {
+        Domain domain = new Domain();
+        domain.setName("Test Domain 1");
+        domain.setPath("/test1");
 
         Optional<Domain> newDomain = domainDAO.createDomain(domain);
         assertTrue(newDomain.isPresent(), "Domain wurde nicht erstellt");
-        assertEquals("Test Domain 1", newDomain.get().getName());
-
-        //Test auf Liste mit Inhalt in beliebiger Reihenfolge
-        // Assertions.assertThat(expectedUserList).containsExactlyInAnyOrderElementsOf(actualUserList);
+        assertEquals(domain.getName(), newDomain.get().getName());
     }
 
 }
